acc58d25e577dd828039feacb680d973991bf60a
Ahmed Mahmoudi <ahmed.mahmoudi@proxym-it.com>
2019-06-19 18:13:31 +0100

    Updated calendar

--- src/controllers/calendar/getCalendarData.js
+++ src/controllers/calendar/getCalendarData.js
@@ -24,28 +24,37 @@ const getCalendarData = async ({ user, db, query }, res) => {
         calendar: []
       }
 
-      let myLeaves = leaves.filter(leave => leave.forUser === userItem.userId);
+      let userLeaves = leaves.filter(leave => leave.forUser === userItem.userId);
+      let userTravels = travels.filter(travel => travel.userId === userItem.userId);
+      let userAuthorizations = authorizations.filter(authorization => authorization.forUser === userItem.userId)
 
       for (let day = parseInt(moment(date).startOf('month').format('D')); day <= parseInt(moment(date).endOf('month').format('D')); day++) {
 
         let currentDate = moment(`${moment(date).month()+1}-${day}-${moment(date).year()}`);
 
-        let authorization;
+        let isDayAuthorization = userAuthorizations.filter(item => currentDate.format('MM-DD-YYYY') === moment(item.date).format('MM-DD-YYYY'));
 
-        let userLeave = leaves.filter(item => (currentDate.format() >= moment(item.dateForm).format() && currentDate.format() <= moment(item.dateTo).format()) && (item.forUser === userItem.userId));
+        let isDayLeave = userLeaves.filter(item => currentDate.format('MM-DD-YYYY') >= moment(item.dateFrom).format('MM-DD-YYYY') && currentDate.format('MM-DD-YYYY') <= moment(item.dateTo).format('MM-DD-YYYY'));
+
+        let isDayTravel = userTravels.filter(item => currentDate.format('MM-DD-YYYY') >= moment(item.startDate).format('MM-DD-YYYY') && currentDate.format('MM-DD-YYYY') <= moment(item.endDate).format('MM-DD-YYYY'));
 
         let isDayHoliday = holidays.filter(holiday => moment(holiday.date).format('DD-MM') === moment(currentDate).format('DD-MM'))
 
         let isDayWeekend = moment(currentDate).format('dddd') === 'Saturday' || moment(currentDate).format('dddd') === 'Sunday' ? true : false;
 
-        console.log([userItem.firstName, currentDate, userLeave.length > 0 ? 1 : 0, isDayHoliday.length > 0 ? 1 : 0, isDayWeekend ? 1 : 0]);
+        console.log([userItem.firstName, currentDate, isDayAuthorization.length > 0 ? 1 : 0, isDayLeave.length > 0 ? 1 : 0, isDayTravel.length > 0 ? 1 : 0, isDayHoliday.length > 0 ? 1 : 0, isDayWeekend ? 1 : 0]);
 
         // console.log('Current date:', currentDate.format('DD-MM-YYYY'), myLeaves.length > 0 ? currentDate.format('DD-MM-YYYY') === moment(myLeaves[0].dateFrom).format('DD-MM-YYYY') : null);
 
+        // console.log("CurrentDate:", currentDate.format(), '\tUserLeaves:', userLeaves);
+        
+        // if (userLeaves.length > 0)  userLeaves.map(item => console.log([currentDate.format('MM-DD-YYYY'), moment(item.dateFrom).format('MM-DD-YYYY'), moment(item.dateTo).format('MM-DD-YYYY')]) );
       }
-      console.log('user leaves', moment().format('DD-MMMM-YYYY'));
 
-      console.log('BZZZ', userCalendar);
+
+      // console.log('user leaves', userLeaves);
+
+      // console.log('BZZZ', userCalendar);
     })
 
     res.status(200).json({
