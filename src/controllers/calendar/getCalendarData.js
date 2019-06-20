f6cf453562d37c2e94ff5e27fbc5db1cc8d05332
Ahmed Mahmoudi <ahmed.mahmoudi@proxym-it.com>
2019-06-20 09:40:09 +0100

    Finalized getCalendarData

--- src/controllers/calendar/getCalendarData.js
+++ src/controllers/calendar/getCalendarData.js
@@ -1,32 +1,29 @@
 import moment from 'moment';
 
-const getCalendarData = async ({ user, db, query }, res) => {
+const getCalendarData = async ({ db, query }, res) => {
 
   console.log("[CALENDAR] ", query);
 
-  const date = moment(query.date).format() || moment();
+  const { dateFilter, groupId } = query;
+
+  const date = moment(dateFilter).format() || moment();
 
   try {
-    const users = await db.collection('users').find({}).toArray();
+    const users = groupId ? await db.collection('users').find({ groupId }).toArray() : await db.collection('users').find({}).toArray();
     const holidays = await db.collection('holidays').find({}).toArray();
     const leaves = await db.collection('leaves').find({}).toArray();
     const travels = await db.collection('travels').find({}).toArray();
     const authorizations = await db.collection('authorizations').find({}).toArray();
 
-    // console.log('All leaves', leaves);
+    let calendar = [];
 
-    // console.log( moment(date).week() );
-    // console.log( moment(moment(date).isoWeek(), 'WW').format('DD-MM-YYYY') );
-    // console.log( moment(moment(date).isoWeek(), 'WW').format('DD-MM-YYYY') );
     users.map(userItem => {
-      let userCalendar = {
-        userId: userItem.userId,
-        calendar: []
-      }
 
       let userLeaves = leaves.filter(leave => leave.forUser === userItem.userId);
       let userTravels = travels.filter(travel => travel.userId === userItem.userId);
-      let userAuthorizations = authorizations.filter(authorization => authorization.forUser === userItem.userId)
+      let userAuthorizations = authorizations.filter(authorization => authorization.forUser === userItem.userId);
+
+      let calendarEntry = [`${userItem.firstName} ${userItem.lastName}`];
 
       for (let day = parseInt(moment(date).startOf('month').format('D')); day <= parseInt(moment(date).endOf('month').format('D')); day++) {
 
@@ -42,28 +39,32 @@ const getCalendarData = async ({ user, db, query }, res) => {
 
         let isDayWeekend = moment(currentDate).format('dddd') === 'Saturday' || moment(currentDate).format('dddd') === 'Sunday' ? true : false;
 
-        console.log([userItem.firstName, currentDate, isDayAuthorization.length > 0 ? 1 : 0, isDayLeave.length > 0 ? 1 : 0, isDayTravel.length > 0 ? 1 : 0, isDayHoliday.length > 0 ? 1 : 0, isDayWeekend ? 1 : 0]);
+        if (isDayWeekend) {
+          calendarEntry.push('W');
+        } else if (isDayHoliday && isDayHoliday.length > 0) {
+          calendarEntry.push('H');
+        } else if (isDayLeave && isDayLeave.length > 0) {
+          calendarEntry.push('L');
+        } else if (isDayTravel && isDayTravel.length > 0) {
+          calendarEntry.push('T');
+        } else if (isDayAuthorization && isDayAuthorization.length > 0) {
+          calendarEntry.push('A');
+        } else {
+          calendarEntry.push('N');
+        }
 
-        // console.log('Current date:', currentDate.format('DD-MM-YYYY'), myLeaves.length > 0 ? currentDate.format('DD-MM-YYYY') === moment(myLeaves[0].dateFrom).format('DD-MM-YYYY') : null);
-
-        // console.log("CurrentDate:", currentDate.format(), '\tUserLeaves:', userLeaves);
-        
-        // if (userLeaves.length > 0)  userLeaves.map(item => console.log([currentDate.format('MM-DD-YYYY'), moment(item.dateFrom).format('MM-DD-YYYY'), moment(item.dateTo).format('MM-DD-YYYY')]) );
       }
 
+      calendar.push(calendarEntry);
+    });
 
-      // console.log('user leaves', userLeaves);
-
-      // console.log('BZZZ', userCalendar);
-    })
-
-    res.status(200).json({
-      first: moment().startOf('month').format('D'),
-      last: moment().endOf('month').format('D'),
-      month: moment().month() + 1
-    })
+    return res.status(200).json({
+      calendar
+    });
   } catch (error) {
-    console.error(error);
+    return res.status(500).json({
+      error
+    });
   }
 
 };
