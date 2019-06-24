7049752762984ef9153fe61f896b53cfed75bc2c
Ahmed Mahmoudi <ahmed.mahmoudi@proxym-it.com>
2019-06-24 09:51:38 +0100

    Updated backend

--- src/controllers/requests/createRequest.js
+++ src/controllers/requests/createRequest.js
@@ -1,126 +1,59 @@
 import uuid from 'uuid/v4';
-import { toLower } from 'lodash';
 import moment from 'moment';
 
 const vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
 
 const createRequest = async ({ user, db, body }, res) => {
-  let userCredit = await db.collection('leaveCredit').findOne({ userId: user.userId });
 
-  const requestBody = {
-    requestId: uuid(),
-    ...body,
-    userCredit: body.requestCategory === 'LEAVE' ? (userCredit ? userCredit.credit : 'not avilable') : null,
-    fromUser: user.userId,
-    fromUserName: `${user.firstName} ${user.lastName}`,
-    userRemainingLeaves: 0,
-    status: "pending",
-    createdAt: moment().format('DD-MM-YYYY H:mm:ss'),
-    createdAtTimestamp: moment().unix()
+  try {
+    const userCredit = await db.collection('leaveCredit').findOne({ userId: user.userId });
+    const userGroup = await db.collection("groups").findOne({ groupId: user.groupId });
+
+    await db.collection('requests').insertOne({
+      requestId: uuid(),
+      ...body,
+      userCredit: body.requestCategory === 'LEAVE' ? (userCredit ? userCredit.credit : 'not avilable') : null,
+      fromUser: user.userId,
+      fromUserName: `${user.firstName} ${user.lastName}`,
+      userRemainingLeaves: 0,
+      status: "pending",
+      createdAt: moment().format('DD-MM-YYYY H:mm:ss'),
+      createdAtTimestamp: moment().unix()
+    });
+
+    if (userGroup) {
+      let title = '';
+
+      if (body.requestCategory === 'ATTENDANCE') {
+        title = `${user.firstName} ${user.lastName} has requested to correct an attendance at ${moment(body.attendance.date).format('DD-MM-YYYY')}.`;
+      } else if (body.requestCategory === 'LEAVE' || body.requestCategory === 'AUTHORIZATION') {
+        title = `${user.firstName} ${user.lastName} has requested a ${body.leaveCategory}${body.leaveCategory.indexOf('leave') !== -1 ? '' : ' leave'}`;
+      } else if (body.requestCategory === 'TRAVEL') {
+        title = `${user.firstName} ${user.lastName} has sent a travel request`;
+      }
+
+      await db.collection('notifications').insertOne({
+        notifId: uuid(),
+        title,
+        content: body.motif,
+        category: body.requestCategory,
+        request: requestBody,
+        targetUser: userGroup.poleLead,
+        vues: [],
+        createdAt: moment().format('DD-MM-YYYY H:mm:ss'),
+        createdAtTimestamp: moment().unix()
+      });
+    }
+
+    return res.status(200).json({
+      message: "Request created successfully."
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error
+    });
   }
 
-  db.collection('requests').insertOne(requestBody, function (err, result) {
-    if (err) {
-      console.log("An error occured.");
-      return res.status(400).json({
-        error: err
-      });
-    } else if (result) {
-
-      if (user.businessRole === 'CEO' || user.businessRole === 'ADMIN') {
-        db.collection("users").findOne({ businessRole: 'CEO' }, (error, ceo) => {
-          if (error) {
-            return res.status(500).json({
-              errorMessage: "Something went wrong."
-            });
-          }
-
-          let title = "";
-
-          if (body.requestCategory === 'ATTENDANCE') {
-            title = `${user.firstName} ${user.lastName} has requested to correct an attendance at ${moment(body.attendance.date).format('DD-MM-YYYY')}.`;
-          } else if (body.requestCategory === 'LEAVE' || body.requestCategory === 'AUTHORIZATION') {
-            title = `${user.firstName} ${user.lastName} has requested a ${body.leaveCategory}${body.leaveCategory.indexOf('leave') !== -1 ? '' : ' leave'}`; 
-            
-            // from ${body.dateFrom} to ${body.dateTo}
-          } else if (body.requestCategory === 'TRAVEL') {
-            title = `${user.firstName} ${user.lastName} has sent a travel request`;
-          }
-
-          db.collection('notifications').insertOne({
-            notifId: uuid(),
-
-            title,
-            content: body.motif,
-            category: body.requestCategory,
-            request: requestBody,
-            targetUser: ceo.userId,
-            vues: [],
-            createdAt: moment().format('DD-MM-YYYY H:mm:ss'),
-            createdAtTimestamp: moment().unix()
-          }, (err, result) => {
-            if (err) {
-              return res.status(500).json({
-                error: err
-              });
-            } else if (result) {
-              return res.status(200).json({
-                message: "Request created successfully."
-              });
-            }
-          });
-
-        });
-      } else {
-        db.collection("groups").findOne({ groupId: user.groupId }, (error, userGroup) => {
-          if (error) {
-            return res.status(500).json({
-              errorMessage: "Something went wrong."
-            });
-          }
-
-          if (userGroup) {
-
-            let title = '';
-
-
-            if (body.requestCategory === 'ATTENDANCE') {
-              title = `${user.firstName} ${user.lastName} has requested to correct an attendance at ${moment(body.attendance.date).format('DD-MM-YYYY')}.`;
-            } else if (body.requestCategory === 'LEAVE' || body.requestCategory === 'AUTHORIZATION') {
-              title = `${user.firstName} ${user.lastName} has requested a ${body.leaveCategory}${body.leaveCategory.indexOf('leave') !== -1 ? '' : ' leave'}`; 
-              
-              // from ${body.dateFrom} to ${body.dateTo}
-            } else if (body.requestCategory === 'TRAVEL') {
-              title = `${user.firstName} ${user.lastName} has sent a travel request`;
-            }
-
-            db.collection('notifications').insertOne({
-              notifId: uuid(),
-              title,
-              content: body.motif,
-              category: body.requestCategory,
-              request: requestBody,
-              targetUser: userGroup.poleLead,
-              vues: [],
-              createdAt: moment().format('DD-MM-YYYY H:mm:ss'),
-              createdAtTimestamp: moment().unix()
-            }, (err, result) => {
-              if (err) {
-                return res.status(500).json({
-                  error: err
-                });
-              } else if (result) {
-                return res.status(200).json({
-                  message: "Request created successfully."
-                });
-              }
-            });
-
-          }
-        })
-      }
-    }
-  });
 };
 
 export default createRequest;
--- src/scheduler/scheduler.js
+++ src/scheduler/scheduler.js
@@ -1,7 +1,6 @@
 import { scheduleJob, RecurrenceRule } from 'node-schedule';
-import moment from 'moment';
-import uuid from 'uuid/v4';
 
+import CheckAbsences from './CheckAbsences';
 import ComputeScores from './ComputeScores';
 import AddLeaveCredits from './AddLeaveCredits';
 
@@ -9,66 +8,16 @@ const scheduler = (db) => {
 
   console.log("Started scheduler.");
 
-  var rule = new RecurrenceRule();
-  rule.hour = 23;
-  rule.minute = 50;
+  // =========================================== Absences =========================================
+  var absenceRule = new RecurrenceRule();
+  absenceRule.hour = 23;
+  absenceRule.minute = 50;
 
-  const checkAbsencesAndNotes = scheduleJob(rule, async function () {
-    // check users absences
+  scheduleJob(absenceRule, () => CheckAbsences(db));
+  // ==============================================================================================
 
-    try {
-
-      const users = await db.collection('users').find({}).toArray();
-      const attendances = await db.collection('attendances').find({}).toArray();
-      const leaves = await db.collection('leaves').find({}).toArray();
-      const travels = await db.collection('travels').find({}).toArray();
-      const holidays = await db.collection('holidays').find({}).toArray();
-
-      if (attendances && attendances.length > 0) {
-
-        users.map(async user => {
-          let isTodayAttendances = attendances.filter(item => item.userId === user.userId && moment(item.date).format('DD-MM-YYYY') === moment().format('DD-MM-YYYY')); //if user has attendances today
-          let isTodayLeave = leaves.filter(item => item.forUser === user.userId && moment(item.dateFrom) <= moment() && moment(item.datoTo) >= moment());
-          let isTodayTravel = travels.filter(item => item.userId === user.userId && moment(item.dateFrom) <= moment() && moment(item.datoTo) >= moment());
-          let isTodayHoliday = holidays.filter(item => moment(item.dateFrom) <= moment() && moment(item.datoTo) >= moment());
-
-          if ((!isTodayAttendances || isTodayAttendances.length < 1) && (!isTodayLeave || isTodayLeave.length < 1) && (!isTodayTravel || isTodayTravel.length < 1) && (!isTodayHoliday || isTodayHoliday.length < 1) && (moment().format('dddd') !== 'Saturday') && (moment().format('dddd') !== 'Sunday')) {
-            await db.collection('absences').insertOne({
-              absenceId: uuid(),
-              absentUser: user.userId,
-              date: moment().format('DD-MM-YYYY')
-            });
-
-            await db.collection("notifications").insertOne({
-              notifId: uuid(),
-              title: `You appear to be absent in ${moment().format('MMMM Do YYYY')}`,
-              content: `You appear to be absent in ${moment().format('MMMM Do YYYY')}, you must fill in an authorization`,
-              category: 'INFO',
-              targetUser: user.userId,
-              vues: [],
-              createdAt: moment().format('DD-MM-YYYY H:mm:ss'),
-              createdAtTimestamp: moment().unix()
-            })
-          }
-        });
-      }
-
-      await db.collection('logs').insertOne({
-        logId: uuid(),
-        origin: 'SCHEDULER',
-        category: 'ABSENCE-CHECKING',
-        date: moment().format('DD-MM-YYYY'),
-        dateTimeStamp: moment().unix(),
-        description: `Checking absences for ${moment().format('DD-MM-YYYY')} done.`
-      });
-
-    } catch (err) {
-      console.error(err);
-    }
-  });
 
   // ========================================= Leave Credits ======================================
-
   var creditsRule = new RecurrenceRule();
   creditsRule.date = 1;
   creditsRule.hour = 0;
@@ -76,10 +25,10 @@ const scheduler = (db) => {
   creditsRule.second = 0;
 
   scheduleJob(creditsRule, () => AddLeaveCredits(db));
+  // ==============================================================================================
 
 
   // ============================================ Scores ==========================================
-
   var scoresRule = new RecurrenceRule();
   scoresRule.dayOfWeek = 6; //On Sunday
   scoresRule.hour = 23;
@@ -87,6 +36,7 @@ const scheduler = (db) => {
   scoresRule.second = 0;
 
   scheduleJob(scoresRule, () => ComputeScores(db));
+  // ==============================================================================================
 }
 
 export default scheduler;
